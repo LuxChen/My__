7126bb870b7b1d0b954895956b583798ac3615e6
chenshuiming <chenshuiming@semir.com>
2023-09-03 12:42:27 +0800

    update

--- test.go
+++ test.go
@@ -3,100 +3,154 @@ package main
 import (
 	"context"
 	"encoding/base64"
-	"encoding/json"
 	"fmt"
+	"io"
 	"log"
+	"net/http"
 	"os"
+	"os/exec"
 	"strings"
 	"time"
 
 	"nhooyr.io/websocket"
 )
 
+func fetchUrl(url string, ch chan string) (string, error) {
+	log.Print(urls[url])
+	resp, err := http.Get(url)
+	if err != nil {
+		ch <- "fail"
+		return "", err
+	}
+
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		ch <- "fail"
+		return "", err
+	}
+	return_string, err := base64.StdEncoding.DecodeString(string(body))
+	if err == nil {
+		ch <- string(return_string)
+		return string(return_string), nil
+	}
+	log.Print("here end_______")
+	ch <- "finish"
+	return "", nil
+}
+
+var urls = map[string]string{
+	"https://raw.fastgit.org/freefq/free/master/v2":                                      "raw",
+	"https://raw.githubusercontent.com/tbbatbb/Proxy/master/dist/v2ray.config.txt":       "raw",
+	"https://github.com/mianfeifq/share":                                                 "code",
+	"https://raw.githubusercontent.com/mahdibland/ShadowsocksAggregator/master/Eternity": "raw",
+}
+
 func main() {
-	// Get the file name.
-	filename := "newlinks.txt"
-	wfile, err := os.OpenFile("my_file.txt", os.O_CREATE, 0644)
+	// ch := make(chan string)
+	// for url := range urls {
+	// 	go fetchUrl(fmt.Sprintf("%v", url), ch)
+	// }
+	// // log.Print(err)
+	// // log.Print(data_str)
+	// allFinish := len(urls)
+	// finished := 0
+	// nodes := ""
+	// for {
+	// 	time.Sleep(time.Millisecond * 1e2)
+	// 	if allFinish == finished {
+	// 		print("finished")
+	// 		break
+	// 	}
+	// 	nam := <-ch
+	// 	print(nam)
+	// 	if nam != "fail" {
+	// 		nodes = nodes + nam
+	// 	}
+	// 	finished++
+	// }
+	// // Split the string.
+	// wfile, _ := os.OpenFile("my_file.txt", os.O_CREATE|os.O_TRUNC, 0644)
+	// slices := strings.Split(nodes, "\n")
+	// t := time.After(time.Second * 5)
+	// // Print the slices.
+	// for _, slice := range slices {
+	// 	p_v := strings.Split(slice, "://")
+	// 	if len(p_v) < 2 {
+	// 		continue
+	// 	}
+	// 	decodedBytes, _ := base64.StdEncoding.DecodeString(p_v[1])
+	// 	switch p_v[0] {
+
+	// 	case "vmess":
+	// 		{
+
+	// 			// Unmarshal the string.
+	// 			var data map[string]interface{}
+	// 			err := json.Unmarshal([]byte(string(decodedBytes)), &data)
+	// 			if err != nil {
+	// 				fmt.Println(err)
+	// 				return
+	// 			}
+
+	// 			// Print the data.
+	// 			address := data["host"]
+	// 			port := fmt.Sprintf("%v", data["port"])
+	// 			if data["path"] == nil || data["path"] == "" {
+	// 				data["path"] = "/"
+	// 			}
+	// 			path := data["path"]
+	// 			go testConn(slice, address, port, path, *wfile)
+	// 		}
+	// 		// case "ss":
+	// 		// 	{
+
+	// 		// 		server := strings.Split(strings.Split(p_v[1], "@")[1], "#")[0]
+	// 		// 		p_g := strings.Split(string(decodedBytes), ":")
+	// 		// 		c, _ := shadowsocks.NewCipher(p_g[0], p_g[1])
+	// 		// 		ss, err := shadowsocks.Dial(server, server, c)
+	// 		// 		if err == nil {
+	// 		// 			_, err := ss.Write([]byte("GET / HTTP/1.1\r\nHost: www.google.com\r\n\r\n"))
+	// 		// 			if err != nil {
+	// 		// 				log.Print(err)
+	// 		// 			}
+	// 		// 			log.Print(slice)
+	// 		// 		}
+	// 		// 		log.Print(err)
+
+	// 		// 	}
+
+	// 	}
+	// }
+	// <-t
+	// cmd := exec.Command("git", "add", "my_file.txt")
+	// stdout, err := cmd.Output()
+
+	// if err != nil {
+	// 	fmt.Println(err.Error())
+	// 	return
+	// }
+
+	// fmt.Print(string(stdout))
+	// cmd = exec.Command("git", "commit", "-m", "update")
+	// stdout, err = cmd.Output()
+
+	// if err != nil {
+	// 	fmt.Println(err.Error())
+	// 	return
+	// }
+
+	// fmt.Print(string(stdout))
+	cmd := exec.Command("git", "push")
+	stdout, err := cmd.Output()
+
 	if err != nil {
-		fmt.Println(err)
+		fmt.Println(err.Error())
 		return
 	}
 
-	// Close the w.
-	defer wfile.Close()
-	// Open the file.
-	file, err := os.Open(filename)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-
-	// Read the file.
-	data := make([]byte, 1024)
-	file_data := ""
-	for {
-		n, err := file.Read(data)
-		if err != nil {
-			break
-		}
-
-		// Close the file.
-
-		// Print the file contents.
-		file_data += string(data[:n])
-	}
-	file.Close()
-	// Split the string.
-	slices := strings.Split(file_data, "\n")
-	t := time.After(time.Second * 5)
-	// Print the slices.
-	for _, slice := range slices {
-		p_v := strings.Split(slice, "://")
-		log.Print(p_v)
-		decodedBytes, _ := base64.StdEncoding.DecodeString(p_v[1])
-		switch p_v[0] {
-
-		case "vmess":
-			{
-
-				// Unmarshal the string.
-				var data map[string]interface{}
-				err := json.Unmarshal([]byte(string(decodedBytes)), &data)
-				if err != nil {
-					fmt.Println(err)
-					return
-				}
-
-				// Print the data.
-				address := data["host"]
-				port := fmt.Sprintf("%v", data["port"])
-				if data["path"] == nil || data["path"] == "" {
-					data["path"] = "/"
-				}
-				path := data["path"]
-				go testConn(slice, address, port, path, *wfile)
-			}
-			// case "ss":
-			// 	{
-
-			// 		server := strings.Split(strings.Split(p_v[1], "@")[1], "#")[0]
-			// 		p_g := strings.Split(string(decodedBytes), ":")
-			// 		c, _ := shadowsocks.NewCipher(p_g[0], p_g[1])
-			// 		ss, err := shadowsocks.Dial(server, server, c)
-			// 		if err == nil {
-			// 			_, err := ss.Write([]byte("GET / HTTP/1.1\r\nHost: www.google.com\r\n\r\n"))
-			// 			if err != nil {
-			// 				log.Print(err)
-			// 			}
-			// 			log.Print(slice)
-			// 		}
-			// 		log.Print(err)
-
-			// 	}
-
-		}
-	}
-	<-t
+	fmt.Print(string(stdout))
 	// Create a WebSocket connection.
 
 }
@@ -106,7 +160,7 @@ func testConn(origin string, address, port, path interface{}, wfile os.File) {
 		ctx    context.Context
 		cancel context.CancelFunc
 	)
-	timeout, err := time.ParseDuration("1.5s")
+	timeout, err := time.ParseDuration("3s")
 	if err == nil {
 		// The request has a timeout, so create a context that is
 		// canceled automatically when the timeout expires.
@@ -135,10 +189,14 @@ func testConn(origin string, address, port, path interface{}, wfile os.File) {
 	if err != nil {
 		log.Print(err)
 	}
-	// Write the string "Hello, world!" to the file.
-	_, err = wfile.WriteString(origin)
-	if err != nil {
-		log.Print(err)
-		return
+
+	_, msg, err := ws.Read(ctx)
+	log.Print("read", err)
+	_, err = wfile.WriteString(origin + "\n")
+	if err != nil && strings.Contains(err.Error(), "context deadline exceeded") {
+		log.Print("here", msg, err)
+	}
+	if err == nil {
+		log.Print("there", msg, err)
 	}
 }
